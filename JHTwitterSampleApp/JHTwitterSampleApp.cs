6ca705194585557deeeaa5329d8462f612a72373
zoomonkey <codingisforyou@outlook.com>
2022-08-20 14:08:53 -0500

    first commit

---
+++ JHTwitterSampleApp/JHTwitterSampleApp.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class JHTwitterSampleApp : Form
+    {
+        static List<TwitterDataModel> result = new List<TwitterDataModel>();
+        static string _twitterBearerToken = ConfigurationManager.AppSettings["TwitterBearerToken"];
+        static string url = "https://api.twitter.com/2/tweets/sample/stream";
+        Thread _backgroundThread = new Thread(new ThreadStart(StartPollingTwitter));
+
+        public JHTwitterSampleApp()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            StartPollingTwitter();
+
+            foreach (TwitterDataModel item in result)
+            {
+                lblMessage.Text += item.data.text + "\n";
+            }
+           
+
+            Console.WriteLine($"The result of Method3 is: {result}");
+        }
+
+
+        public static void StartPollingTwitter()
+        {
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Accept = "application/json";
+            httpRequest.Headers["Authorization"] = "Bearer " + _twitterBearerToken;
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var sr = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                string line;
+                int count = 0;
+                // Read line by line  
+                while ((line = sr.ReadLine()) != null)
+                {
+                    count++;
+
+                    TwitterDataModel myDeserializedClass = JsonConvert.DeserializeObject<TwitterDataModel>(line);
+
+                    result.Add(myDeserializedClass);
+                    if (count > 10)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ JHTwitterSampleApp/TwitterDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class Data
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+    }
+
+    public class TwitterDataModel
+    {
+        public Data data { get; set; }
+    }
+
+}
